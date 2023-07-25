19969e4bf114efadac1c7e908f6754a0de92d9dd
Stephen Grier <git@grier.org.uk>
2023-07-26 00:40:35 +0100

    Delete unused files

--- terraform/modules/pagerduty-user/user.tf
+++
@@ -1,7 +0,0 @@
-resource "pagerduty_user" "user" {
-  name      = "${var.name}"
-  email     = "${var.email}"
-  role      = "${var.role}"
-  job_title = "${var.job_title}"
-  teams     = ["${var.teams}"]
-}
